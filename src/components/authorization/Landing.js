a04909e2c374ca53de0880b2bbcd803e7b792cf1
Rachel Meyerchick <rachelmeyerchick@Rachels-MacBook-Pro.local>
2019-06-13 10:49:41 -0500

    fullstack

---
+++ src/components/authorization/Landing.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Landing extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    
+    
+    render() {
+        return (
+            <div>
+                Landing
+            </div>
+        )
+    }
+}
+
+export default Landing;
---
+++ src/components/authorization/Login.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                Login
+            </div>
+        )
+    }
+}
+
+export default Login;
